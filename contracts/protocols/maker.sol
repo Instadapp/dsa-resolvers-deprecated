e005e2b345783b2bf1981c5a88953944f631970e
Mubaris NK <mubaris@instadapp.io>
2021-03-31 18:11:17 +0530

    Update version

--- contracts/protocols/maker.sol
+++ contracts/protocols/maker.sol
@@ -275,5 +275,5 @@ contract DSRResolver is VaultResolver {
 
 
 contract InstaMakerResolver is DSRResolver {
-    string public constant name = "Maker-Resolver-v1.2";
+    string public constant name = "Maker-Resolver-v1.3";
 }
